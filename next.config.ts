de86ea07deedcad4d52274c5cfe680cc22637f42
Alyson <sistema@nedcapital.com.br>
2026-07-29 15:47:14 -0300

    fix: corrige rota e forca atualizacao da pagina principal

--- next.config.ts
+++ next.config.ts
@@ -23,7 +23,8 @@ const nextConfig: NextConfig = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          // AQUI: 'Authorization' adicionado no final da string abaixo 👇
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
         ],
       },
     ];
